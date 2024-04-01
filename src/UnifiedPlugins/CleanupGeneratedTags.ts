c597536546fd4059622fdc6c3cb957fcfa597c93
Kodewerfer <lijleen@gmail.com>
2024-04-01 16:32:56 -0400

    Added a counter for better debugging

--- src/UnifiedPlugins/CleanupGeneratedTags.ts
+++ src/UnifiedPlugins/CleanupGeneratedTags.ts
@@ -2,7 +2,11 @@ import {visit} from 'unist-util-visit'
 import {remove} from 'unist-util-remove';
 
 function TagsCleanupTransformer(ast: object) {
-    visit<any, any>(ast, 'element', Visitor)
+    let CleanupCount = 0;
+    visit<any, any>(ast, 'element', Visitor);
+    if (CleanupCount > 0) {
+        console.log("Cleanup Plugin: generated tags removed:", CleanupCount)
+    }
     
     function Visitor(node: any, index: any, parent: any) {
         
@@ -10,6 +14,7 @@ function TagsCleanupTransformer(ast: object) {
         
         if (NodeProps['dataIsGenerated']) {
             remove(parent, node);
+            CleanupCount += 1;
             return;
         }
         
