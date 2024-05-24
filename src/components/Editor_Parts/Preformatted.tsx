3214c343c9b483b179e89fb2430b6c2f88f26769
Kodewerfer <lijleen@gmail.com>
2024-05-24 18:40:15 -0400

    complete overhaul using the new standard

--- src/components/Editor_Parts/Preformatted.tsx
+++ src/components/Editor_Parts/Preformatted.tsx
@@ -3,10 +3,11 @@
  * for a code element to be a "CodeItem", it must be under a pre element and have the correct attrs
  */
 
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useLayoutEffect, useRef, useState} from "react";
 import {TDaemonReturn} from "../../hooks/useEditorHTMLDaemon";
-import {GetCaretContext, GetChildNodesAsHTMLString, TextNodeProcessor,} from "../Helpers";
+import {GetCaretContext, GetChildNodesAsHTMLString, GetChildNodesTextContent, TextNodeProcessor,} from "../Helpers";
 import dedent from "dedent";
+import {TActivationReturn} from "../Editor_Types";
 
 //
 export function Preblock({children, tagName, parentSetActivation, daemonHandle, ...otherProps}: {
@@ -18,25 +19,10 @@ export function Preblock({children, tagName, parentSetActivation, daemonHandle, 
 }) {
     const ContainerRef = useRef<HTMLElement | null>(null);
     
-    const [isBlockEmpty, setIsBlockEmpty] = useState(false);
-    
-    // Add a simple Br as filler element if no preformatted item
-    // No really needed because the current deletion functions will delete the block all-together
-    useEffect(() => {
-        if (!children || React.Children.count(children) === 1) {
-            if (String(children).trim() === '' && ContainerRef.current) {
-                setIsBlockEmpty(true);
-                ContainerRef.current = null;
-            }
-        }
-    });
-    
-    const FillerElement = (<br/>);
-    
     return React.createElement(tagName, {
         ref: ContainerRef,
         ...otherProps
-    }, isBlockEmpty ? [FillerElement] : children);
+    }, children);
 }
 
 // Only handle code blocks, inline codes are PlainSyntax component
@@ -48,91 +34,126 @@ export function CodeItem({children, tagName, daemonHandle, ...otherProps}: {
     daemonHandle: TDaemonReturn; // replace Function with a more specific function type if necessary
     [key: string]: any; // for otherProps
 }) {
-    const [SetActivation] = useState<(state: boolean) => void>(() => {
-        return (state: boolean) => {
-            
-            if (!state) {
-                ElementOBRef.current?.takeRecords();
-                ElementOBRef.current?.disconnect();
-                ElementOBRef.current = null;
-                
-                if (CodeElementRef.current && CodeElementRef.current.textContent
-                    && CodeElementRef.current.parentNode && CodeElementRef.current.parentNode.nodeName.toLowerCase() === 'pre') {
-                    
-                    // Added by the unified plugin
-                    const syntaxData = otherProps['data-md-syntax'];
-                    
-                    let ReplacementNode = null;
-                    
-                    // NOTE: converter's quirk, element will still be converted even if the ending half of the syntax is missing
-                    // NOTE: due to the particularity of the pre element(can contain syntax that should be converted to element),
-                    // only send to convert if only the result will still be a pre
-                    if (syntaxData
-                        && CodeElementRef.current.textContent.startsWith(syntaxData)
-                        && CodeElementRef.current.textContent.endsWith(syntaxData)) {
-                        
-                        const textNodeResult = TextNodeProcessor(CodeElementRef.current.textContent);
-                        ReplacementNode = textNodeResult?.length ? textNodeResult[0] : null;
-                    } else {
-                        ReplacementNode = document.createElement('p') as HTMLElement;
-                        ReplacementNode.innerHTML = GetChildNodesAsHTMLString(CodeElementRef.current?.childNodes);
-                    }
-                    
-                    
-                    if (ReplacementNode) {
-                        daemonHandle.AddToOperations({
-                            type: "REPLACE",
-                            targetNode: CodeElementRef.current.parentNode,
-                            newNode: ReplacementNode //first result node only
-                        });
-                        daemonHandle.SyncNow();
-                    }
-                }
-            }
-            if (state) {
-                daemonHandle.SyncNow();
-            }
-            
-            setIsEditing(state);
-            return {
-                "enter": EnterKeyHandler,
-                "del": DelKeyHandler,
-            }
-        }
+    const [SetActivation] = useState<(state: boolean) => TActivationReturn>(() => {
+        return ComponentActivation;
     }); // the Meta state, called by parent via dom fiber
     const [isEditing, setIsEditing] = useState(false);
     
     const CodeElementRef = useRef<HTMLElement | null>(null);
+    const SyntaxFillerFront = useRef<HTMLElement | null>(null);
+    const SyntaxFillerRear = useRef<HTMLElement | null>(null);
+    
+    const TextBlocksMapRef = useRef(new Map<HTMLElement | Node, boolean>()); // all possible text blocks under the code element
+    const CodeLangTextRef = useRef(""); // the "language" that displayed after ```
     
     const ElementOBRef = useRef<MutationObserver | null>(null);
     
-    // Show when actively editing
-    const [GetEditingStateChild] = useState(() => {
-        return () => {
-            const textContent = CodeElementRef.current?.firstChild?.textContent?.trim();
-            
-            let codeElementClassName = CodeElementRef.current?.className;
-            let CodeLang = null;
-            if (codeElementClassName) {
-                
-                CodeLang = codeElementClassName.split(' ').find(c => {
-                    return codeElementClassName?.startsWith("language-");
-                });
-                
-            }
-            
-            if (!CodeLang) CodeLang = "";
-            
-            if (CodeLang && CodeLang.startsWith("language-")) CodeLang = CodeLang.split("language-")[1];
-            
-            return dedent`\`\`\` ${CodeLang}
-                    ${textContent}
-                    \`\`\``;
+    // Added by the unified plugin
+    const syntaxData = otherProps['data-md-syntax'] || '';
+    
+    function ComponentActivation(state: boolean): TActivationReturn {
+        const componentHandlers = {
+            "enter": EnterKeyHandler,
+            "backspaceOverride": BackspaceHandler,
+            "delOverride": DelKeyHandler,
         };
-    });
+        
+        if (!state) {
+            ElementOBRef.current?.takeRecords();
+            ElementOBRef.current?.disconnect();
+            ElementOBRef.current = null;
+            
+            if (CodeElementRef.current && CodeElementRef.current.textContent
+                && CodeElementRef.current.parentNode && CodeElementRef.current.parentNode.nodeName.toLowerCase() === 'pre') {
+                
+                const NewCodeContent = CompileAllTextNode()?.replace('\u00A0', '');
+                if (!NewCodeContent) return componentHandlers;
+                UpdateCodeElement(NewCodeContent);
+            }
+        }
+        if (state) {
+            daemonHandle.SyncNow();
+            
+            if (typeof MutationObserver) {
+                ElementOBRef.current = new MutationObserver(ObserverHandler);
+                CodeElementRef.current && ElementOBRef.current?.observe(CodeElementRef.current, {
+                    childList: true,
+                    subtree: true
+                });
+            }
+        }
+        
+        setIsEditing(state);
+        return componentHandlers
+    }
+    
+    function ObserverHandler(mutationList: MutationRecord[]) {
+        mutationList.forEach((Record) => {
+            if (!Record.removedNodes.length) return;
+            
+            Record.removedNodes.forEach((Node) => {
+                if (TextBlocksMapRef.current.get(Node) || Node === SyntaxFillerFront.current || Node === SyntaxFillerRear.current) {
+                    
+                    if (CodeElementRef.current && CodeElementRef.current.textContent
+                        && CodeElementRef.current.parentNode && CodeElementRef.current.parentNode.nodeName.toLowerCase() === 'pre') {
+                        
+                        const NewCodeContent = CompileAllTextNode()?.replace('\u00A0', '');
+                        if (NewCodeContent)
+                            UpdateCodeElement(NewCodeContent);
+                    }
+                }
+            })
+        })
+    }
+    
+    function CompileAllTextNode() {
+        if (!CodeElementRef.current) return;
+        let elementWalker = document.createTreeWalker(CodeElementRef.current, NodeFilter.SHOW_TEXT);
+        
+        let node;
+        let textContent = '';
+        while (node = elementWalker.nextNode()) {
+            textContent += node.textContent;
+        }
+        
+        return textContent.trim();
+    }
+    
+    function UpdateCodeElement(NewCodeContent: string) {
+        
+        if (!CodeElementRef.current || !CodeElementRef.current.parentNode) return;
+        
+        let ReplacementNode = null;
+        
+        // NOTE: converter's quirk, element will still be converted even if the ending half of the syntax is missing
+        // NOTE: due to the particularity of the pre element(can contain syntax that should be converted to element),
+        // only send to convert if only the result will still be a pre
+        if (NewCodeContent.startsWith(syntaxData) && NewCodeContent.endsWith(syntaxData)) {
+            
+            const textNodeResult = TextNodeProcessor(NewCodeContent);
+            ReplacementNode = textNodeResult?.length ? textNodeResult[0] : null;
+        } else {
+            ReplacementNode = document.createElement('p') as HTMLElement;
+            const textNode = document.createTextNode(GetChildNodesTextContent(CodeElementRef.current?.childNodes));
+            ReplacementNode.appendChild(textNode);
+        }
+        
+        
+        if (ReplacementNode) {
+            daemonHandle.AddToOperations({
+                type: "REPLACE",
+                targetNode: CodeElementRef.current.parentNode,
+                newNode: ReplacementNode //first result node only
+            });
+            daemonHandle.SyncNow();
+        }
+    }
+    
+    function BackspaceHandler(ev: Event) {
+        ev.stopImmediatePropagation();
+    }
     
     function DelKeyHandler(ev: Event) {
-        ev.preventDefault();
         ev.stopImmediatePropagation();
     }
     
@@ -150,30 +171,76 @@ export function CodeItem({children, tagName, daemonHandle, ...otherProps}: {
         
         // Add a new line after, use generic logic from editor
         if (TextAfterSelection?.trim() === "" || !TextAfterSelection)
-            return true;
+            return false;
         
         
         return;
     }
     
+    // keep track of code's language
+    useLayoutEffect(() => {
+        if (CodeElementRef.current) {
+            const codeElementClassName = CodeElementRef.current.className;
+            let CodeLang = null;
+            if (codeElementClassName)
+                CodeLang = codeElementClassName.split(' ').find(c => {
+                    return codeElementClassName?.startsWith("language-");
+                });
+            
+            if (!CodeLang) CodeLang = "";
+            if (CodeLang && CodeLang.startsWith("language-")) CodeLang = CodeLang.split("language-")[1];
+            
+            CodeLangTextRef.current = CodeLang.trim();
+        }
+        
+        return () => {
+            CodeLangTextRef.current = '';
+        }
+    });
+    
     // change contentEditable type to plaintext-only
     useEffect(() => {
-        GetEditingStateChild();
+        // GetEditingStateChild();
         // add code element itself to ignore
         if (CodeElementRef.current) {
             CodeElementRef.current.contentEditable = "plaintext-only";
             daemonHandle.AddToIgnore(CodeElementRef.current, "any");
         }
-        // Add all child element to ignore
+        // Add all child element to ignore, add all text nodes to TextBlocksMapRef
         if (CodeElementRef.current?.childNodes) {
-            CodeElementRef.current.childNodes.forEach(node => {
-                daemonHandle.AddToIgnore(node, "any");
-            })
+            daemonHandle.AddToIgnore(Array.from(CodeElementRef.current.childNodes), "any", true);
+            
+            Array.from(CodeElementRef.current.childNodes).forEach((child) => {
+                if (child.nodeType === Node.TEXT_NODE) {
+                    TextBlocksMapRef.current.set(child as HTMLElement, true);
+                }
+            });
+        }
+        
+        return () => {
+            TextBlocksMapRef.current.clear();
         }
     });
     
     return React.createElement(tagName, {
         ...otherProps,
         ref: CodeElementRef,
-    }, isEditing ? GetEditingStateChild() : children);
+    }, [
+        <span className={`Text-Normal Whole-Line ${isEditing ? "" : 'Hide-It'}`}
+              data-is-generated={true}
+              key={'SyntaxFront'}>
+            <span contentEditable={false} ref={SyntaxFillerFront}>{syntaxData}</span>
+            {CodeLangTextRef.current === "" ? '\u00A0' : CodeLangTextRef.current}
+            {'\n'}
+        </span>,
+        
+        ...(Array.isArray(children) ? children : [children]),
+        
+        <span className={`Text-Normal Whole-Line ${isEditing ? "" : 'Hide-It'}`}
+              data-is-generated={true}
+              key={'SyntaxRear'}>
+            <span contentEditable={false} ref={SyntaxFillerRear}>{syntaxData}</span>
+        </span>,
+    ]);
+    
 }
