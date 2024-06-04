b6c8007553429ef52932d637ad1240fc8aea9822
Kodewerfer <lijleen@gmail.com>
2024-06-04 00:00:47 -0400

    added more comment

--- src/UnifiedPlugins/CleanupExtraText.ts
+++ src/UnifiedPlugins/CleanupExtraText.ts
@@ -4,8 +4,11 @@ import {remove} from "unist-util-remove";
 function TextCleanUpTransformer(ast: object) {
     visit<any, any>(ast, 'text', Visitor)
     
+    // in the current logic, there should not be any text nodes directly under the "body" of the editor,
+    // Thus they're unhandled, and will be non-editable if appeared. this plug delete all of them
     // TODO: this will remove "\n" text, maybe buggy, need more testing
     function Visitor(node: any, index: any, parent: any) {
+        
         if (parent.type === "root") {
             // console.log("cleaning node:", node);
             remove(node);
