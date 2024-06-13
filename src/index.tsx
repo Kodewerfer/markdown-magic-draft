7d818fc3709cd121133dd1f098ae8ef0e596c162
Kodewerfer <lijleen@gmail.com>
2024-06-13 00:07:46 -0400

    file cleanups

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Test_APP from './Test_APP';
-import reportWebVitals from './reportWebVitals';
+// @ts-ignore
+import Test_App from './Test_App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Test_APP />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Test_App/>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
