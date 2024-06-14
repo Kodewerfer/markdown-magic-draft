54742fd72d1e495823d079a71729b0934927d1bf
Kodewerfer <lijleen@gmail.com>
2024-06-13 20:38:37 -0400

    More cleanup for package conversion, Added building for Editor component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx,css}",
-    "./public/index.html"
-  ],
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'class',
+    content: [
+        "./src/**/*.{js,jsx,tsx,css}"
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    }
 }
 
